abd3d145148669ac291dcd42048330cdabd69eac
adil <adil.business4064@gmail.com>
2025-11-20 16:32:06 +0530

    async await

---
+++ hello-async/src/main.rs
@@ -0,0 +1,32 @@
+use trpl::{Either,Html};
+
+fn main() {
+    let args=std::env::args().collect();
+
+    trpl::run(async {
+        let title_1=page_title(&args[1]);
+        let title_2=page_title(&args[2]);
+
+        let (url,maybe_title)=
+            match trpl::race(title_1,title_2).await {
+                Either::Left(left)=>left,
+                Either::Right(right)=>right,
+            };
+
+        println!("{url} returned first.");
+
+        match maybe_title {
+            Some(title)=>println!("its page title was: '{title}'"),
+            None=>println!("it had no title."),
+        }
+    })
+}
+
+async fn page_title(url:&str)->(&str,Option<String>){
+    let response_text=trpl::get(url).await.text().await;
+    let title=Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html());
+
+    (url,title)
+}
